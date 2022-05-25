8ccf16dc59515cd176c98e9916ffe8b8d9a54795
UIT-21522600 <95834050+ngocthanh2705@users.noreply.github.com>
2022-05-25 22:42:41 +0700

    Update main.cpp
    Thêm struct điểm tọa độ

--- main.cpp
+++ main.cpp
@@ -8,7 +8,26 @@
 #include<windows.h>
 using namespace std;
 
-struct DIEM {};
+struct DIEM {
+  int x, y;
+	DIEM() {
+		x = 0;
+		y = 0;
+	}
+	DIEM(int a, int b) {
+		x = a;
+		y = b;
+	}
+	DIEM operator - (DIEM d2) {
+		DIEM d;
+		d.x = x - d2.x;
+		d.y = y - d2.y;
+		return d;
+	}
+	bool operator == (DIEM d2) {
+		return (x == d2.x && y == d2.y);
+	}
+};
 struct RAN {};
 struct MOI {};
 struct KHUNG {};
