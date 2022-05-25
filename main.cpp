d9fb417fb22c8b053c73f945280f17e27022f0cb
UIT-21522600 <95834050+ngocthanh2705@users.noreply.github.com>
2022-05-25 22:40:16 +0700

    Create main.cpp
    Create main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+#include<vector>
+#include<ctime>
+#include<cstring>
+#include<windows.h>
+using namespace std;
+
+struct DIEM {};
+struct RAN {};
+struct MOI {};
+struct KHUNG {};
+
+int main(){
+
+
+}
