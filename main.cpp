434d2b724b80d928717abc0118025fc0f18d4bd0
UIT-21522527 <105805810+UIT-21522527@users.noreply.github.com>
2022-05-27 23:59:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,7 +29,32 @@ struct DIEM {
 	}
 };
 struct RAN {};
-struct MOI {};
+struct MOI {
+	DIEM toado;
+	bool BiRanAn(vector<DIEM> toadoran) {
+		return (toadoran[0] == toado);
+	}
+	bool MoiTrenRanHoacKhongHopLi(vector<DIEM> toadoran) {
+		for (int i = 0; i < toadoran.size(); i++) {
+			if (toado == toadoran[i])
+				return true;
+			if (i != 0) {
+				int xtrong = (toadoran[i].x + toadoran[i - 1].x) / 2;
+				int ytrong = (toadoran[i].y + toadoran[i - 1].y) / 2;
+				if (toado.x == xtrong && toado.y == ytrong)
+					return true;
+			}
+		}
+		return false;
+	}
+	void TaoMoi(vector<DIEM> toadoran, int chieudai, int chieurong) {
+		srand((unsigned)time(NULL));
+		do {
+			toado.x = rand() % (chieurong - 2) + 1;
+			toado.y = rand() % (chieudai - 2) + 1;
+		} while (MoiTrenRanHoacKhongHopLi(toadoran));
+	}
+};
 struct KHUNG {};
 
 int main(){
