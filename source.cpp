aa6f79cc3e5f750adbb65587e95865bb94ca929e
UIT-21522662 <94778997+UIT-215222662@users.noreply.github.com>
2022-05-28 22:06:56 +0700

    Update source.cpp
    use own code

--- source.cpp
+++ source.cpp
@@ -1,70 +1,34 @@
-#include <iostream>
-#include <windows.h>
-#include <cstdlib>
-#include <conio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+#include<vector>
+#include<ctime>
+#include<cstring>
+#include<windows.h>
 using namespace std;
-void gotoxy( int column, int line );
-struct Point{
-    int x,y;
-};
-class CONRAN{
-public:
-    struct Point A[100];
-    int DoDai;
-    CONRAN(){
-        DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
-        A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
-    }
-    void Ve(){
-        for (int i = 0; i < DoDai; i++){
-            gotoxy(A[i].x,A[i].y);
-            cout<<"X";
-        }
-    }
-    void DiChuyen(int Huong){
-        for (int i = DoDai-1; i>0;i--)
-            A[i] = A[i-1];
-        if (Huong==0) A[0].x = A[0].x + 1;
-        if (Huong==1) A[0].y = A[0].y + 1;
-        if (Huong==2) A[0].x = A[0].x - 1;
-        if (Huong==3) A[0].y = A[0].y - 1;
+int main() {
+	hear: KHUNG khung;
+	NhapCacYeuCau(khung);
+	khung.Vekhung();
+	khung.ran.ToaDo.resize(khung.ran.ChieuDai);
+	khung.VeTroChoi();
+	_getch();
+	system("cls");
+	string a = "\n\n\n\n\n\n\n\n                                              Do you want to play again? \n                                                       yes/no";
+	cout << a << endl;
+	cout << "                                                Your answer: ";
+	string m;
+	cin >> m;
+	if (m == "yes")
+	{
+		system("cls");
+		goto hear;
+	}
 
-    }
-};
-//hehe
-int main()
-{
-    CONRAN r;
-    int Huong = 0;
-    char t;
-
-    while (1){
-        if (kbhit()){
-            t = getch();
-            if (t=='a') Huong = 2;
-            if (t=='w') Huong = 3;
-            if (t=='d') Huong = 0;
-            if (t=='x') Huong = 1;
-        }
-        system("cls");
-        r.Ve();
-        r.DiChuyen(Huong);
-        Sleep(300);
-    }
-
-    return 0;
+	string s = "\n                                                     Thank you";
+	for (int a[8] = { 10,11,12,14,11,10,14,12 }, i = 0; i < s.length(); i++) {
+		SetConsoleTextAttribute(khung.h, a[i % 8]);
+		cout << s[i];
+	}
 }
-
-
-void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
-    );
-  }
