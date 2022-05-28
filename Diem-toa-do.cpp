2baf4bad5e7b4e37bca7657d74d574e96273547b
UIT-21522600 <95834050+ngocthanh2705@users.noreply.github.com>
2022-05-28 21:49:45 +0700

    Create Diem-toa-do.cpp
    Tạo struct Điểm tọa độ

---
+++ Diem-toa-do.cpp
@@ -0,0 +1,20 @@
+struct DIEM {
+	int x, y;
+	DIEM() {
+		x = 0;
+		y = 0;
+	}
+	DIEM(int a, int b) {
+		x = a;
+		y = b;
+	}
+	DIEM operator - (DIEM d2) {
+		DIEM d;
+		d.x = x - d2.x;
+		d.y = y - d2.y;
+		return d;
+	}
+	bool operator == (DIEM d2) {
+		return (x == d2.x && y == d2.y);
+	}
+};
